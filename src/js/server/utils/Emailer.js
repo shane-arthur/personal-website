380d20116f654b2f6c557e4e805357d51ed75347
Shane Arthur <shane.arthur@gmail.com>
2017-06-06 22:21:44 -0230

    changes before night.

--- src/js/server/utils/Emailer.js
+++ src/js/server/utils/Emailer.js
@@ -1,10 +1,11 @@
 import nodemailer from 'nodemailer';
 import { Promise } from 'bluebird';
+let url = '';
 
 export const mailer = {
     sendMail: (sender, email, text) => {
         const mailOptions = { from: sender, to: 'shane.arthur@gmail.com', subject: `<${email}> : Personal Website Email`, text: text };
-        const transporter = nodemailer.createTransport('smtps://shane.arthur@gmail.com:Dallas4mavs8@smtp.gmail.com');
+        const transporter = nodemailer.createTransport(url);
         return new Promise((resolve, reject) => {
            // transporter.sendMail(mailOptions).then(() => {
                 resolve(`Email successfully sent to ${email}`);
