d1fb4447198700b1052b1df408058c8a584ef4b3
Shane Arthur <shane.arthur@gmail.com>
2017-06-09 09:35:10 -0230

    fix broken build.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: 'http://localhost:'+devServerPort+'/dist/'
     },
